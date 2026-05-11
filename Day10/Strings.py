cad1f4d6a663169fa4807ca1cada07fb668654b0
Sairam Merugumalla <sairammerugumalla@gmail.com>
2026-05-11 14:25:55 +0530

    updated till Day10 - strings

--- Day10/Strings.py
+++ Day10/Strings.py
@@ -1,7 +1,76 @@
-# Strings in python 
-
-
-
 # Python.org ----> reference 
+# pythontutor --->  for understanding the excecution flow of the program step by step 
 
-# pythontutor ---> 
+
+# Strings in python
+
+# strings are immutable sequence (ordered collection) ---> indexing 
+# immutable ---> once it is created , it can not be changed 
+# supports unicode characters(international characters)
+
+a = "Rocky"
+A = "rocky"
+
+name = "sairam"
+
+Name ='Rocky'
+
+msg ="""iam a python full stack developer.Also python Trainer.Im a superman.Im a flash man."""
+print(msg)
+
+print(len(name))
+print(len(msg))
+
+
+print("python" + " Training")  # concatination
+
+print("hi " * 3) # repetation 
+
+
+
+# Strings Methods : inbuilt functions avaiable for string objects 
+
+name = "sairam"
+print(name.capitalize())  # only the first character of the first word will be uppercase and remaining everything will be lowercase 
+
+print(msg.capitalize())
+
+print(msg.title()) # first letter of each and word will be uppercase 
+
+print() 
+uname = name.upper() 
+print(uname)
+
+print(msg.replace("python","SQl"))
+
+k = "Entertainment"
+print(k.find("e"))  
+
+
+
+
+# Indexing 
+# slicing 
+# string Formatting 
+
+# functions 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
